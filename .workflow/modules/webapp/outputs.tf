34f7c3e69af16bb46f3e21f812b4cef1458acdef
Oskar <oskartdragon@gmail.com>
2024-02-21 07:01:58 +0000

    feat changes

--- .workflow/modules/webapp/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "s3_url" {
-  description = "S3 hosting URL (HTTP)"
-  value       = aws_s3_bucket_website_configuration.bucket.website_endpoint
-}
---
+++ build/modules/webapp/cert.tf
@@ -0,0 +1,43 @@
+# resource "aws_acm_certificate" "cert" {
+#   domain_name       = var.domain
+#   validation_method = "DNS"
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+# resource "aws_acm_certificate_validation" "cert_validation" {
+#   certificate_arn         = aws_acm_certificate.cert.arn
+#   validation_record_fqdns = [aws_route53_record.root.fqdn]
+# }
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.environment_name == "prod" ? "${var.domain}" : "${var.environment_name}.${var.domain}"
+  validation_method = "DNS"
+}
+
+
+resource "aws_route53_record" "cert" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = local.dns_zone_id
+}
+
+
+resource "aws_acm_certificate_validation" "cert" {
+  depends_on = [aws_acm_certificate.cert, aws_route53_record.cert]
+
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert : record.fqdn]
+}
